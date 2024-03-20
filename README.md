745942481584d0747be5544baedaa67526e304c0
Dionna <businessladythick@yahoo.com>
2024-03-19 22:45:35 -0400

    got to figure out why my logs say null

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ i started with a new page that i am going to change up to my 307 main page lab
 i created new folders, folders within that also have pages and images
 
 adding a structure that will allow me to use the requirements in the assignment
+
+modified some codes
+changed my top-menu
+added a background picture
+
+added button to say what customer's favorite flavor is when they type it in
+
+styled my page cute but plain
--- script/index.js
+++ script/index.js
@@ -1,15 +1,32 @@
+console.log('this is working');
+
+
 var menuLinks = [
     { text: 'about', href: '/about' },
     { text: 'orders', href: '/orders' },
     { text: 'account', href: '/account' },
   ];
 
-const flavors = ['Kiwi', 'Sponge Bob', 'Orange Cream', 'Red Lips', 'Blue Lips', 'Strawberry', 'Grape', 'Blueberry'];
+const flavors = ['Kiwi', 'Sponge Bob', 'Orange Cream', 'Red Lips', 'Blue Lips', 'Strawberry', 'Grape', 'Blueberry', 'Chocolate', 'Coffee', 'Cotton Candy', 'cherry'];
 const submenuItems = document.querySelector('.submenu-items');
+console.log(submenuItems);
+console.log(submenuItems.parentElement);
+
+const flavorsMenuEl = document.querySelector('.submenu-items');
+console.log(flavorsMenuEl);
+
+//class list & i con't get this to work :(
+flavorsMenuEl.addEventListener('click', () => {
+    submenuItems.classList.toggle('visible');
+    
+  });
 
 flavors.forEach(function(flavor) {
   const liEl = document.createElement('li');
+  console.log(liEl);
+
   const aEl = document.createElement('a');
+  console.log(aEl);
 
   aEl.href = `#${flavor.toLowerCase().replace(/ /g, '-')}`;
   aEl.textContent = flavor;
@@ -25,14 +42,15 @@ flavors.forEach(function(flavor) {
 
 const mainEl = document.getElementById('h-1');
 console.log(mainEl);
-mainEl.style.color = 'red';
+mainEl.style.color = 'black';
 mainEl.style.textAlign ='center'; 
 
 let pEl = document.querySelector('p'); 
 console.log(pEl);
+console.log(NodeList);
 
-pEl.innerHTML = '<strong>50 + <br> FLAVORS</strong>';
-
+//modify the HTML content
+pEl.innerHTML = '<strong>A Cool Treat <br> Fit For Any Day</strong>';
 
 
 for (let i = 0; i < menuLinks.length; i++) {
@@ -44,9 +62,6 @@ var aEl = document.createElement('a');
 //add href attribute w/it's value of the href property of the 'link' object
  aEl.href = link.href;
 
-//set new element content to the value of the text property of the 'link' object
-//  aEl.textContent = link.text;/
-
 //append new element to the topMenuEl
  document.body.appendChild(aEl);
 
@@ -56,10 +71,13 @@ mainEl.innerHTML = "<h1> DEEZ COLD ONES <br> SNOW CONES </h1>";
 mainEl.style.textAlign = "center";
 mainEl.classList.add("flex-ctr");
 
+//getElementById & parent element
 const topMenuEl = document.getElementById('top-menu');
+const parentEl = topMenuEl.parentEl;
 console.log(topMenuEl);
-topMenuEl.style.height = '50px';
-topMenuEl.style.backgroundColor = '#ffff00';
+
+topMenuEl.style.height = '60px';
+topMenuEl.style.backgroundColor = '#faf0dc';
 topMenuEl.classList.add('flex-around');
 
 //iterate over menuLinks array and for each 'link' object
@@ -67,95 +85,33 @@ for (let i = 0; i < menuLinks.length; i++) {
     var link = menuLinks[i];
     
 //create an <a> element
-var aEl = document.createElement('a');
-
-//add href attribute w/it's value of the href property of the 'link' object
-// aEl.href = link.href;
-
-//set new element content to the value of the text property of the 'link' object
-// aEl.textContent = link.text;
+let aEl = document.createElement('a');
+console.log(aEl);
 
 //append new element to the topMenuEl
 topMenuEl.prepend(aEl);  //had to google/append put the array to the side of pg/'PREPEND' puts at top
 }
 
-//  let mainEl = document.querySelector('main');
-// console.log(mainEl);
-
-// mainEl.innerHTML = "<h1> DEEZ COLD ONES SNOW CONES </h1>";
-// mainEl.style.textAlign = "center";
-// mainEl.classList.add("flex-ctr");
-
-
-
-//const topMenuEl = document.getElementById('top-menu');
-//console.log(topMenuEl);
-//topMenuEl.style.height = '100%';
-//topMenuEl.style.backgroundColor = '#0e9aa7';
-//topMenuEl.classList.add('flex-around');
-
-
-// const navEl = document.getElementById('.navbar');
-// console.log(navEl);
-// 
-
-// navEl.style.height = '100%';
-// navEl.style.backgroundColor = '#9ACD32';
-// navEl.classList.add('flex-around');
-// 
-// for (var i = 0; i < menuLinks.length; i++) {
-    // var link = menuLinks[i];
-    
-//create an <a> element
-// var aEl = document.createElement('a');
-
-//add href attribute w/it's value of the href prope
-// aEl.href = link.href;
-
-//set new element content to the value of the text 
-// aEl.textContent = link.text;
-
-//append new element to the topMenuEl
-// navEl.prepend(aEl);  //had to google/append put
-// }
-
-
-//let aTag = document.createElement('a');
-//aTag.textContent = "visit google"; 
-//aTag.setAttribute('href', 'https://www.google.com');
-//console.log(aTag);
-
-//document.body.appendChild(h2El);
-
-
-// const pQuery = document.querySelector('p');
-// console.log(pQuery);
-// 
-// let listEl = document.querySelectorAll('li');
-// console.log(listEl);
-// 
-// listEl = document.querySelectorAll('.comment');
-// console.log(listEl);
-// 
-// listEl.forEach(listItem => {
-// console.log(listItem);
-// console.log(listItem.textContent);
-// console.log(listItem.parentNode); 
-// });
-// 
-// document.querySelector('ul')
-// .addEventListener('click', handleBtnClick);
-// 
-// function handleBtnClick(evt) {
-    // console.log(evt.target);
-    // let evtColor = evt.target.style.color;
-    // console.log(evtColor);
-    // if (evt == 'red') {
-        // evt.target.style.color = 'black';
-    // } else {
-        // evt.target.style.color ='red';
-    // }
-// }
-// 
-// 
-
+document.addEventListener("DOMContentLoaded", function() {
+    let input = document.querySelector("input[type='text']");
+    let button = document.querySelector("button");
+  
+    button.addEventListener("click", function() {
+      const value = input.value;
+      processInput(value);
+    });
+  
+    //event listener with associated event handler function & input
+    input.addEventListener("keydown", function(event) {
+      if (event.keyCode === 13) {
+        const value = input.value;
+        processInput(value);
+      }
+    });
+  
+    function processInput(value) {
+      // Do something with the input value, e.g. display it on the page
+      alert("You entered: " + value);
+      console.log(processInput);
+    }
+  });
